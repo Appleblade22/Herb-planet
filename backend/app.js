21f70392855203836dd31dc250a6055dc73a4dad
APPLE <appleblade22@gmail.com>
2023-09-23 19:59:33 +0530

    Done

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,15 @@ import axios from "axios";
 import multer from "multer";
 import bodyParser from "body-parser";
 import FormData from "form-data";
+import cors from "cors";
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
@@ -17,7 +20,9 @@ app.post("/upload", upload.single("file"), async (req, res) => {
     try{
         //create a new form data
         const identificationRequest = new FormData();
-        const fileBlob = new Blob([req.file.buffer], { type: req.file.mimetype });
+        
+        
+        const fileBlob = Buffer.from(req.file.buffer, "base64");
         identificationRequest.append("organs", req.body.organs);
         identificationRequest.append("images", fileBlob, req.file.originalname);
         const identificationResponse = await axios.post(`https://my-api.plantnet.org/v2/identify/all?include-related-images=true&no-reject=false&lang=en&api-key=2b10y4v0e005Y3nzwf9zYpOsO`, 
